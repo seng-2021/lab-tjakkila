749cced4c43da81ead18f1d67b74e2551b776341
tjakkila <60666923+tjakkila@users.noreply.github.com>
2022-01-20 22:11:26 +0200

    Modified decode()

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -16,6 +16,8 @@ def encode(s):
                 raise ValueError
             if c.islower():
                 c=c.upper()
+            elif c.isupper():
+                c=c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
@@ -30,35 +32,7 @@ def encode(s):
     return crypted
 
 def decode(s):
-    start_time = time.time()
-    if not isinstance(s,str):
-        raise TypeError
-    origlen = len(s)
-    crypted = ""
-    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
-
-    if len(s) > 1000:
-        raise ValueError
-    for c in s:
-        if c in "äåö":
-            raise ValueError
-        if c.isalpha():
-            if c.isupper():
-                c=c.lower()
-            # Rot13 the character for maximum security
-            crypted+=codecs.decode(c,'rot13')
-        elif c in digitmapping:
-          crypted+=digitmapping[c]
-        else:
-            raise ValueError
-    i = origlen
-    while (True):
-        current_time = time.time()
-        if ((current_time - start_time) > 0.05):
-            break
-
-
-    return crypted
+    return encode(s)
 
 
 
